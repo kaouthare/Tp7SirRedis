c24cbd193fc51268ce808c6a0db709d2b7de2488
kaouthar <kaou.bennouna@gmail.com>
2016-03-24 19:24:51 +0100

    exemple 3

--- src/App.java
+++ src/App.java
@@ -3,10 +3,24 @@ import redis.clients.jedis.Jedis;
 public class App
 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
+		String cacheKey = "cachekey";
 		Jedis jedis = new Jedis("localhost");
-		System.out.println(jedis.get("counter"));
-		jedis.incr("counter");
-		System.out.println(jedis.get("counter"));
+		// adding a new key
+		jedis.set(cacheKey, "cached value");
+		// setting the TTL in seconds
+		jedis.expire(cacheKey, 15);
+		// Getting the remaining ttl
+		System.out.println("TTL:" + jedis.ttl(cacheKey));
+		Thread.sleep(1000);
+		System.out.println("TTL:" + jedis.ttl(cacheKey));
+		// Getting the cache value
+		System.out.println("Cached Value:" + jedis.get(cacheKey));
+
+		// Wait for the TTL finishs
+		Thread.sleep(15000);
+
+		// trying to get the expired key
+		System.out.println("Expired Key:" + jedis.get(cacheKey));
 	}
 }
